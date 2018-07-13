e0b303cd7da1e9c8b12b86156ded086f7757fa31
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-07-13 15:10:36 +0100

    Using absolute path to access auxiliary files
    Using quotation marks to protect against spaces in the absolute path.

--- install_dhcp_and_dns.sh
+++ install_dhcp_and_dns.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Set script variables
+AUXILIARY_FILES_PATH=$(echo "$(pwd)/$0" | grep -P ".*/" -o)
+AUXILIARY_FILES_PATH="${AUXILIARY_FILES_PATH%/}"  # Remove the last "/" from the string
+
 # Configure the machine network settings until reboot (assumes that no DHCP is available)
 sudo ifconfig eth0 192.168.1.254/24
 sudo route add default gateway 192.168.1.1
@@ -8,21 +12,21 @@ sudo su -c 'echo "nameserver 1.1.1.1" >> /etc/resolv.conf'
 # Install the necessary packages to automatically configure the machine network settings on boot
 sudo apt-get update
 sudo apt-get -y install ifupdown resolvconf
-sudo su -c "cat etc-network-interfaces >> /etc/network/interfaces"
+sudo su -c "cat \"$AUXILIARY_FILES_PATH/etc-network-interfaces\" >> /etc/network/interfaces"
 
 # Install the DNS and DHCP servers
 sudo apt-get install -y bind9 isc-dhcp-server
 
 sudo perl -i -p -e "s|INTERFACESv4=\"\"|INTERFACESv4=\"eth0\"|" /etc/default/isc-dhcp-server
-sudo cp -f dhcp/dhcpd.conf /etc/dhcp/
-sudo cp -f dhcp/isc-dhcp-server /etc/network/if-up.d/
+sudo cp -f "$AUXILIARY_FILES_PATH/dhcp/dhcpd.conf" /etc/dhcp/
+sudo cp -f "$AUXILIARY_FILES_PATH/dhcp/isc-dhcp-server" /etc/network/if-up.d/
 
 sudo /usr/sbin/rndc-confgen -a
-sudo cp -f dns/named.conf.options /etc/bind/
-sudo cp -f dns/named.conf.local /etc/bind/
-sudo cp -f dns/db.home /etc/bind/
+sudo cp -f "$AUXILIARY_FILES_PATH/dns/named.conf.options" /etc/bind/
+sudo cp -f "$AUXILIARY_FILES_PATH/dns/named.conf.local" /etc/bind/
+sudo cp -f "$AUXILIARY_FILES_PATH/dns/db.home" /etc/bind/
 sudo chown bind:bind /etc/bind/db.home
-sudo cp -f dns/db.1.168.192.rev /etc/bind/
+sudo cp -f "$AUXILIARY_FILES_PATH/dns/db.1.168.192.rev" /etc/bind/
 sudo chown bind:bind /etc/bind/db.1.168.192.rev
 sudo chown bind:bind /etc/bind
 
